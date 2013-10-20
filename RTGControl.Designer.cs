e0ea114f93e79bab790c828cae1daee3f15fc6ed
Yang Hu <gumuyueying@gmail.com>
2013-10-20 14:59:49 +0800

    完成控件的布局操作。
    One Panel and five PictureBox.

--- RTGControl.Designer.cs
+++ RTGControl.Designer.cs
@@ -28,10 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.pbAxisY = new System.Windows.Forms.PictureBox();
+            this.pbTitle = new System.Windows.Forms.PictureBox();
+            this.pbRight = new System.Windows.Forms.PictureBox();
+            this.pbAxisX = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelGraph.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAxisY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAxisX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.Controls.Add(this.pictureBox1);
+            this.panelGraph.Controls.Add(this.pbAxisX);
+            this.panelGraph.Controls.Add(this.pbRight);
+            this.panelGraph.Controls.Add(this.pbTitle);
+            this.panelGraph.Controls.Add(this.pbAxisY);
+            this.panelGraph.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGraph.Location = new System.Drawing.Point(0, 0);
+            this.panelGraph.Margin = new System.Windows.Forms.Padding(0);
+            this.panelGraph.MaximumSize = new System.Drawing.Size(400, 300);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(400, 300);
+            this.panelGraph.TabIndex = 0;
+            // 
+            // pbAxisY
+            // 
+            this.pbAxisY.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pbAxisY.Location = new System.Drawing.Point(0, 0);
+            this.pbAxisY.Margin = new System.Windows.Forms.Padding(0);
+            this.pbAxisY.Name = "pbAxisY";
+            this.pbAxisY.Size = new System.Drawing.Size(80, 240);
+            this.pbAxisY.TabIndex = 0;
+            this.pbAxisY.TabStop = false;
+            // 
+            // pbTitle
+            // 
+            this.pbTitle.Location = new System.Drawing.Point(80, 0);
+            this.pbTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.pbTitle.Name = "pbTitle";
+            this.pbTitle.Size = new System.Drawing.Size(280, 40);
+            this.pbTitle.TabIndex = 1;
+            this.pbTitle.TabStop = false;
+            // 
+            // pbRight
+            // 
+            this.pbRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbRight.Location = new System.Drawing.Point(360, 0);
+            this.pbRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pbRight.Name = "pbRight";
+            this.pbRight.Size = new System.Drawing.Size(40, 240);
+            this.pbRight.TabIndex = 2;
+            this.pbRight.TabStop = false;
+            // 
+            // pbAxisX
+            // 
+            this.pbAxisX.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbAxisX.Location = new System.Drawing.Point(0, 240);
+            this.pbAxisX.Margin = new System.Windows.Forms.Padding(0);
+            this.pbAxisX.Name = "pbAxisX";
+            this.pbAxisX.Size = new System.Drawing.Size(400, 60);
+            this.pbAxisX.TabIndex = 3;
+            this.pbAxisX.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(80, 40);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 200);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RTGControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelGraph);
+            this.Name = "RTGControl";
+            this.Size = new System.Drawing.Size(400, 300);
+            this.panelGraph.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAxisY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAxisX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelGraph;
+        private System.Windows.Forms.PictureBox pbTitle;
+        private System.Windows.Forms.PictureBox pbAxisY;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbAxisX;
+        private System.Windows.Forms.PictureBox pbRight;
     }
 }
