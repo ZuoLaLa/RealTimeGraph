880dcb78c368aee2475f2f5330906912953a512a
Yang Hu <gumuyueying@gmail.com>
2014-01-21 09:30:45 +0800

    Add basic data class.

---
+++ DataRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace RealTimeGraph
+{
+    class DataRange
+    {
+        public float Min { get; set; }
+        public float Max { get; set; }
+
+        public DataRange(float min, float max)
+        {
+            if (min > max)
+            {
+                throw new ArgumentException("Min can not be larger than max.");
+            }
+            else
+            {
+                Min = min;
+                Max = max;
+            }
+        }
+    }
+}
---
+++ DataRect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealTimeGraph
+{
+    class DataRect
+    {
+        private DataRange axisX;
+        private DataRange axisY;
+
+        public float XMin
+        {
+            get { return axisX.Min; }
+        }
+
+        public float XMax
+        {
+            get { return axisX.Max; }
+        }
+
+        public float YMin
+        {
+            get { return axisY.Min; }
+        }
+
+        public float YMax
+        {
+            get { return axisY.Max; }
+        }
+
+        public DataRect(float xMin, float xMax, float yMin, float yMax)
+        {
+            axisX = new DataRange(xMin, xMax);
+            axisY = new DataRange(yMin, yMax);
+        }
+    }
+}
